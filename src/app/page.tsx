be3eeff2522d46d5a25cba51d60553b4e6fdc0c5
Pedro Pimpão <pedrohmpimpao@gmail.com>
2025-12-19 10:28:41 -0300

    chore: Change base colors and add shadcn component for test and add Prisma orm

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./_components/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+    <h1>Hello, World</h1>
+    <p>Teste</p>
+    <Button>
+      BUTAO
+    </Button>
+    </>
+  );
+}
