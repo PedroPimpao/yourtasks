9c26ba150b45cf47c5bc8be338ca99b9234d2e14
Pedro Pimpão <pedrohmpimpao@gmail.com>
2025-12-19 12:11:05 -0300

    chore: Prettier, Prisma and Better Auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { Button } from "./_components/ui/button";
 
-
 export default function Home() {
   return (
     <>
-    <h1>Hello, World</h1>
-    <p>Teste</p>
-    <Button>
-      BUTAO
-    </Button>
+      <h1>Hello, World</h1>
+      <p>Teste</p>
+
+      <Button>BUTAO</Button>
     </>
   );
 }
