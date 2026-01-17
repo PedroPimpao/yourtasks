ddd39713e8b7df064e8e30bbb8557892e44b85b0
Pedro Pimpão <pedrohmpimpao@gmail.com>
2026-01-17 17:33:52 -0300

    chore: add fields on task table schema

---
+++ prisma/migrations/20260117200220_change_task_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "task" ADD COLUMN     "inProcess" BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20260117203242_change_task_table_2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "task" ADD COLUMN     "isPending" BOOLEAN NOT NULL DEFAULT true;
