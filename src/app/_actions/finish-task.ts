de0c7ae346667f644d0ed5777197247b27884fbd
Pedro Pimpão <pedrohmpimpao@gmail.com>
2026-01-21 15:28:11 -0300

    feat: add start and finish task functions

--- src/app/_actions/finish-task.ts
+++ src/app/_actions/finish-task.ts
@@ -1,1 +1,20 @@
 "use server"
+
+import { db } from "@/src/lib/prisma"
+
+interface FinishTaskProps {
+    taskID: string
+}
+
+export const finishTask = async ({ taskID }: FinishTaskProps) => {
+    await db.task.update({
+        where: {
+            id: taskID
+        },
+        data: {
+            isCompleted: true,
+            inProcess: false,
+            isPending: false
+        }
+    })
+}
--- src/app/_actions/start-task.ts
+++ src/app/_actions/start-task.ts
@@ -1,1 +1,20 @@
 "use server"
+
+import { db } from "@/src/lib/prisma"
+
+interface StartTaskProps {
+    taskID: string
+}
+
+export const startTask = async ({ taskID } : StartTaskProps) => {
+    await db.task.update({
+        where: {
+            id: taskID
+        },
+        data: {
+            isCompleted: false,
+            inProcess: true,
+            isPending: false
+        }
+    })
+}
